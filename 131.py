29d6b547045442e37bbe3c252684f52903334ded
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-07-27 18:45:44 +0530

    Add files via upload

--- 131.py
+++ 131.py
@@ -2,19 +2,21 @@ import plotly.express as px
 
 name=[]
 import csv
-with open("merged1.csv",'r') as f:
+with open("dwarf_stars.csv",'r') as f:
   f1=csv.reader(f)
   for a  in f1:
     name.append(a)
-
+star_data=name[1:]
 gravity=[]
-  
 mass=[]
 radius=[]
 name=[]
+for i in star_data:
+  mass.append(i[3])
+  radius.apeend(i[4])
+  name.append(i[1])
+
 for index,name in enumerate(name):
   g=(float(mass[index]) * 5.972e+24)/(float(radius[index]) * float(radius[index]) * (6371000**2)) *6.674e-11
   gravity.append(g) 
 
-fig=px.scatter(x=radius,y=mass,size=gravity,hover_data=[name])
-fig.show()
