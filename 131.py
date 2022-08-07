80b5001542088c8541b9449a606b43e674b1165d
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-08-07 18:10:38 +0530

    Add files via upload

--- 131.py
+++ 131.py
@@ -1,12 +1,12 @@
 import plotly.express as px
-
-name=[]
+import pandas as pd
+name1=[]
 import csv
-with open("dwarf_stars.csv",'r') as f:
-  f1=csv.reader(f)
+with open("dwarf_stars.csv",'r+') as file:
+  f1=csv.reader(file)
   for a  in f1:
-    name.append(a)
-star_data=name[1:]
+    name1.append(a)
+star_data=name1[1:]
 gravity=[]
 mass=[]
 radius=[]
@@ -14,13 +14,16 @@ star_name=[]
 for i in star_data:
   mass.append(star_data[3])
   radius.append(star_data[4])
-  name.append(star_data[1])
+  name1.append(star_data[1])
 
 star_gravity=[]
 
 for index,name in enumerate(star_name):
   g=(float(mass[index])) *6.17/100000000000/(float(radius[index]) * float(radius[index]) ) 
+  print(g)
   #g=(float(mass[index])*6.17/1000) /(float(radius[index]) * float(radius[index]) ) 
-  star_gravity.append(g) 
+  star_gravity.append(g)
+  
+  
+file.to_csv("star_with_gravity.csv",index=False) 
 
-f.to_csv("star_with_gravity.csv")
