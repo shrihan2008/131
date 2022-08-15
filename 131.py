ac0055fdfcfcfff5cc69b1de5f8b771b32eaa143
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-08-15 13:45:44 +0530

    Add files via upload

--- 131.py
+++ 131.py
@@ -1,7 +1,4 @@
-def convert_to_si(radius,mass): 
-  for i in range(0,len(radius)-1): 
-    radius[i] = radius[i]*6.957e+8 
-    mass[i] = mass[i]*1.989e+30 
+
 
 import plotly.express as px
 import pandas as pd
@@ -12,6 +9,11 @@ star_data=name1[1:]
 gravity=[]
 mass=f['Mass'].to_list()
 radius =f['Radius'].to_list()
+for i in range(0,len(radius)-1): 
+    radius[i] = radius[i]*6.957e+8 
+    mass[i] = mass[i]*1.989e+30 
+
+
 star_name=[]
 for i in star_data:
   mass.append(star_data[3])
@@ -21,10 +23,11 @@ for i in star_data:
 star_gravity=[]
 
 for index,name in enumerate(star_name):
-  g=convert_to_si(radius,mass)
+  g=(float(gravity))*float(mass)/float(radius**2)
   print(g)
-  #g=(float(mass[index])*6.17/1000) /(float(radius[index]) * float(radius[index]) ) 
   star_gravity.append(g)
+
+print(len(star_gravity))
   
   
 f.to_csv("star_with_gravity.csv",index=False) 
